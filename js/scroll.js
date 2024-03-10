ca6374651858c1b38e8cf0da384b1cd2b3b31a98
Flavio Scheck <flavio.scheck@gmail.com>
2024-03-10 16:29:42 -0300

    add scroll effect

---
+++ js/scroll.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Obtener lista de todos los elementos afectados
+  const sections = document.querySelectorAll('.fade'); 
+
+  function handleScroll(element) {
+      const windowHeight = window.innerHeight;
+
+      // Calculamos la posición del elemento en relación con la ventana
+      const elementPosition = element.getBoundingClientRect().top;
+
+      if (elementPosition < windowHeight * 0.85) {
+          // Si el elemento está cerca de la parte superior de la ventana, añadimos la clase fadeIn
+          element.classList.remove('fadeOut');
+          element.classList.add('fadeIn');
+      } else {
+          // Si el elemento no está cerca de la parte superior de la ventana, añadimos la clase fadeOut
+          element.classList.remove('fadeIn');
+          element.classList.add('fadeOut');
+      }
+  }
+
+  // Evento de desplazamiento
+  sections.forEach(section => {
+      window.addEventListener('scroll', function () {
+          handleScroll(section);
+      });
+
+      handleScroll(section);
+  });
+});
